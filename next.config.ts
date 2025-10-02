dc29712fcba66b4d62dc77fdd4b6445b05337161
Efter-26 <efterjahanema@gmail.com>
2025-10-02 18:28:16 +0600

    update for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'deenfashionbd.com',
         port: '',
         pathname: '/public/storage/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
